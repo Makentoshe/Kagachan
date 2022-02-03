c0ba3924b9b0bdc0a94557b64b7ef0a15cbcd56b
Makentoshe <mkliverout@gmail.com>
2022-02-03 20:15:25 +0300

    feat: implement wallpaper change for Android platform

--- android/src/main/java/com/makentoshe/android/MainActivity.kt
+++ android/src/main/java/com/makentoshe/android/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.makentoshe.android
 
-import com.makentoshe.common.App
+import android.app.WallpaperManager
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.toBitmap
+import com.makentoshe.common.App
+import com.makentoshe.common.wallpaper.AndroidWallpaper
+import com.makentoshe.common.wallpaper.AndroidWallpaperSetter
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent { MaterialTheme { App() } }
+
+        val wallpaperManager = WallpaperManager.getInstance(this)
+        val drawable = ContextCompat.getDrawable(this, R.drawable.sas)!!
+        AndroidWallpaperSetter(wallpaperManager).setup(AndroidWallpaper(drawable.toBitmap()))
     }
+
 }
