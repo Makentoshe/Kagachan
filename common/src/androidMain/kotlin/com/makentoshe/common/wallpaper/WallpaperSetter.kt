847442731d896a7ce37ebfb860721442b8bbd4e6
Makentoshe <mkliverout@gmail.com>
2022-02-03 18:17:03 +0300

    feat: implement WallpaperSetter for android platform

--- common/src/androidMain/kotlin/com/makentoshe/common/wallpaper/WallpaperSetter.kt
+++ common/src/androidMain/kotlin/com/makentoshe/common/wallpaper/WallpaperSetter.kt
@@ -1,14 +1,18 @@
 package com.makentoshe.common.wallpaper
 
+import android.app.WallpaperManager
 import android.graphics.Bitmap
+import java.io.IOException
 
 actual interface WallpaperSetter<T : Any> {
     actual fun setup(wallpaper: Wallpaper<T>): Boolean
 }
 
-class AndroidWallpaperSetter : WallpaperSetter<Bitmap>{
-    override fun setup(wallpaper: Wallpaper<Bitmap>): Boolean {
-
-        return true
+class AndroidWallpaperSetter(private val wallpaperManager: WallpaperManager) : WallpaperSetter<Bitmap> {
+    override fun setup(wallpaper: Wallpaper<Bitmap>): Boolean = try {
+        wallpaperManager.setBitmap(wallpaper.get())
+        true
+    } catch (ioe: IOException) {
+        false
     }
 }
