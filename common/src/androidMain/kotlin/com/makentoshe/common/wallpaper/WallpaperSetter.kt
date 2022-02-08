b16fb23d4e85c321486a4be9f017ab298eea7445
Makentoshe <mkliverout@gmail.com>
2022-02-08 19:33:20 +0300

    feat: implement WallpaperResult as a result of the set wallpaper feature

--- common/src/androidMain/kotlin/com/makentoshe/common/wallpaper/WallpaperSetter.kt
+++ common/src/androidMain/kotlin/com/makentoshe/common/wallpaper/WallpaperSetter.kt
@@ -2,17 +2,18 @@ package com.makentoshe.common.wallpaper
 
 import android.app.WallpaperManager
 import android.graphics.Bitmap
+import com.makentoshe.common.functional.Either
 import java.io.IOException
 
 actual interface WallpaperSetter<T : Any> {
-    actual fun setup(wallpaper: Wallpaper<T>): Boolean
+    actual fun setup(wallpaper: Wallpaper<T>): WallpaperResult
 }
 
 class AndroidWallpaperSetter(private val wallpaperManager: WallpaperManager) : WallpaperSetter<Bitmap> {
-    override fun setup(wallpaper: Wallpaper<Bitmap>): Boolean = try {
+    override fun setup(wallpaper: Wallpaper<Bitmap>): WallpaperResult = try {
         wallpaperManager.setBitmap(wallpaper.get())
-        true
-    } catch (ioe: IOException) {
-        false
+        Either.Left(Unit)
+    } catch (e: Exception) {
+        Either.Right(e)
     }
 }
